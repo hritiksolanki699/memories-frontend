eeee18a594d048eb9a2d97211eafabba68338bb3
Hritik Solanki <hritiksolanki699@gmail.com>
2023-01-11 13:41:13 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // "http://localhost:5000/posts" ||
-const url =  "https://memories-backend-seven.vercel.app";
+const url =  "https://memories-backend-seven.vercel.app/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
