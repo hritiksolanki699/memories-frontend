f622de998f1bfb5fd632bbc8cdb2821effed58ee
Hritik Solanki <hritiksolanki699@gmail.com>
2023-01-11 13:15:15 +0530

    first commit

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import moment from "moment";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import {useDispatch} from 'react-redux'
+
+import { deletePost, likePost } from "../../../actions/posts";
+
+const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        borderRadius: "15px",
+        height: "100%",
+        position: "relative",
+      }}
+    >
+      <CardMedia
+        sx={{
+          height: 0,
+          paddingTop: "56.25%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          backgroundBlendMode: "darken",
+        }}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "20px",
+          left: "20px",
+          color: "white",
+        }}
+      >
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="h6">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "20px",
+          right: "20px",
+          color: "white",
+        }}
+      >
+        <Button
+          sx={{ color: "white" }}
+          size="small"
+          onClick={() => setCurrentId(post._id)}
+        >
+          <MoreHorizIcon />
+        </Button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "20px",
+        }}
+      >
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography
+        sx={{ padding: "0 16px" }}
+        variant="h5"
+        color="textSecondary"
+        gutterBottom
+      >
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component='p'
+          gutterBottom
+        >
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          padding: "0 16px 8px 16px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
+          <ThumbUpIcon fontSize="small" />&nbsp;
+          Like&nbsp;{post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Post;
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@mui/material";
+
+const Posts = ({setCurrentId}) => {
+  const posts = useSelector((state) => state.posts);
+
+  return !posts.length ? <CircularProgress /> 
+  : 
+  <Grid 
+  sx={{ display: 'flex',
+  alignItems: 'center'}}
+  container
+  alignItems="stretch" 
+  spacing={3}
+  >
+  {
+    posts.map((post) =>(
+      <Grid key={post._id} item xs={12} sm={12} md={6}>
+         <Post post={post} setCurrentId={setCurrentId} />
+      </Grid>
+    ))
+  }
+  </Grid>;
+};
+
+export default Posts;
