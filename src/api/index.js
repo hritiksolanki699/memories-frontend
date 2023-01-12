fb2adaabf321639808dda092659c0881d00e8f94
Hritik Solanki <hritiksolanki699@gmail.com>
2023-01-12 14:39:11 +0530

    form validation

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const url = "https://memories-backend-seven.vercel.app/posts";
+const url = "https://memories-backend-seven.vercel.app/posts";
 
-const url = "http://localhost:5000/posts" || "https://memories-backend-seven.vercel.app/posts";
+// const url = "http://localhost:5000/posts" || "https://memories-backend-seven.vercel.app/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
